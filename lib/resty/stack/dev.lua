f407122d4b3928bf84c5deddbde518aaf63c1b16
Anton Heryanto <anton.heryanto@gmail.com>
2015-03-18 11:34:09 +0800

    improve dev using direct file concat instead

--- lib/resty/stack/dev.lua
+++ lib/resty/stack/dev.lua
@@ -1,119 +1,67 @@
 -- Copyright (C) Anton heryanto.
 
-local split = require "resty.stack.utils".split
+local split = require "resty.stack.string".split
 local type = type
+local open = io.open
 local popen = io.popen
 local concat = table.concat
 local sub = string.sub
 local gsub = string.gsub
-local capture = ngx.location.capture
-local say = ngx.print
-local var = ngx.var
+local len = string.len
+local print = ngx.print
 local log = ngx.log
+local WARN = ngx.WARN
 local req = ngx.req
+local prefix = ngx.config.prefix
+local root = prefix() .. '../'
 
-local ok, config = pcall(require, "config")
-if not ok then 
-    log(ngx.WARN, 'config.lua fail to load')
-    config = {} 
-end
-
-local function cmd_unix(path, ext)
+local function concat_file(path, ext, fn, fn_after)
     local cmd = 'find '.. path ..' -name "*.'.. ext ..'"'
     local i = 0 -- avoid ifinite loop
-    local result
+    local output = ''
+    local start = len(path) + 2
+    local stop = -2 - len(ext)
     while true do
         local handler,err = popen(cmd)
         if err or handler == nil then 
-            log(ngx.WARN, "handler invalid ", err) 
+            log(WARN, "handler invalid ", err) 
             break 
         end
 
-        result = handler:read('*a')
-        handler:close()
-        if result or i == 100 then break end
-        log(ngx.INFO, "result empty ", i)
-        i = i + 1
-    end
+        local total = 0
+        local success = 0
+        for file in handler:lines() do
+            total = total + 1
+            local h,e = open(file,'r')
+            local o = h:read("*all")
+            h:close()
+            if not h or e then break end
 
-    return result
-end
-
-local function cmd_windows(path, ext)
-    local dir = concat(split(path,'/'),'\\')
-    local cmd = 'forfiles /p '.. dir ..' /s /m *.'.. ext ..
-    ' /c "cmd /c echo @relpath"'
-    local i, raw = 0
-
-    while true do
-        local handler = popen(cmd)
-        raws = handler:read('*a')
-        handler:close()
-        if raw or i == 100 then break end
-        i = i + 1
-    end
-
-    local files = split(raws,'\n')
-    local result = ''
-    for i=1,#files do
-        local file = files[i]
-        if file ~= "" then
-            result = result .. path ..'/'.. 
-            concat(split(sub(file,4,-2),'\\'),'/') ..'\n'
+            local name = sub(file,start,stop)
+            output = output .. (fn and fn(name, o) or o)
+            success = success + 1
         end
-    end
-    return result
-end
-
-local cmd = cmd_unix
-if ok and config.is_windows then cmd = cmd_windows end
-
-local function combine(path, base, ext, fn, fn_after)
-    local result = cmd(path, ext or "js")
-
-    if not result then
-        log(ngx.WARN, "popen failed ")
-        return
-    end
-
-    fn = fn or function(file, uri, body) return body end
-    local files = split(result,'\n')
-    local n = #files
-    local strip = base and base:len() + 1 or nil
-    local output = ''
-    for i=1,n do
-        local file = files[i]
-        if file ~= "" then
-            local uri = strip and sub(file,strip) or '/'.. file
-            local res = capture(uri)
-            if res.status == 200 then
-                output = output .. fn(file, uri, res.body)
-            end
-        end
+        handler:close()
+        if total == success or i == 100 then break end
+        log(WARN, "result empty ", i)
+        i = i + 1
     end
     output = fn_after and fn_after(output) or output
-    say(output)
+    print(output)
 end
-
-local function html2js(path, base)
-    combine(path, base, 'html', function(file, uri, body)
-        local len = path:len() + 2
-        return "  '".. sub(file,len,-6) .."':'".. gsub(body,'%s+',' ') .."',\n"
-    end, function(o)
-    return 'window.templates = {\n'.. o .. '};\n'
-end)
-end
-
 
 local args = req.get_uri_args()
-local path = (args.path or 'app') ..'/'
+local path = root .. (args.path or 'app') ..'/'
 local base = not args.path and 'app' or nil 
 local html = not args.html and 'views' or type(args.html) == 'table' and args.html[1] or args.html
 local js = not args.js and {'presenters'} or type(args.js) ~= 'table' and {args.js} or args.js
 
---templates
-html2js(path.. html, base) 
---js
-for i=1,#js do 
-    combine(path.. js[i], base) 
-end
+
+for i=1,#js do concat_file(path.. js[i], 'js') end
+-- template
+concat_file(path .. html, 'html', function(name, value) 
+    return "  '".. name .."':'".. gsub(value,'%s+',' ') .."',\n"
+end, function(o) 
+    return 'window.templates = {\n'.. o .. '};\n'
+end)
+
